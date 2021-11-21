bdbddec4fe02881d9ecd24024d26390a2425c5ba
Jay <me@wjay.kim>
2021-11-21 22:21:12 +0900

    fix(ios): use topmost view controller
    Add logic to find topmost presenting view controller.
    This fixes problem ad is not showing when current presenting controller is differ from RCTPresentedViewController.
    
    Refs: #59

--- ios/Ads/RNAdMobFullScreenAd.swift
+++ ios/Ads/RNAdMobFullScreenAd.swift
@@ -23,7 +23,18 @@ class RNAdMobFullScreenAd<T>: NSObject {
     }
     
     func getViewController(reject: RCTPromiseRejectBlock?) -> UIViewController? {
-        let viewController = RCTPresentedViewController()
+        var viewController = RCTKeyWindow()?.rootViewController
+        while true {
+            if let presented = viewController?.presentedViewController {
+                viewController = presented
+            } else if let nav = viewController as? UINavigationController {
+                viewController = nav.visibleViewController
+            } else if let tab = viewController as? UITabBarController {
+                viewController = tab.selectedViewController
+            } else {
+                break
+            }
+        }
         if (viewController == nil && reject != nil) {
             reject!("E_NIL_VC", "Cannot process Ad because the current View Controller is nil.", nil)
         }
